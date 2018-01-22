f951bfa04c62152ca503e0cad57de9d0913d52b9
akvrdata <34913074+akvrdata@users.noreply.github.com>
2018-01-22 15:21:30 +0530

    desc

--- src/main/scala/scalaprojects.scala
+++ src/main/scala/scalaprojects.scala
@@ -7,16 +7,13 @@ object scalaprojects
   def main(args:Array[String])
   {
 
-    val conf = new SparkConf().setAppName("scalaprojects").setMaster("local")
+    val conf = new SparkConf().setAppName("scalaprojects").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val file = sc.textFile("/user/arunvr4912/listofwords")
     val wordsrdd = file.flatMap(x=>x.split(" "))
     val wordsrddkv = wordsrdd.map(x=>(x,1))
     val reduce = wordsrddkv.reduceByKey((x,y)=>(x+y))
-
-
-
   }
 }
 
